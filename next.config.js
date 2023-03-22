4ef5e601ce74b31baf68bc219f7262d4fd351dbf
Jol0o <jloyd9836@gmail.com>
2023-03-22 13:52:52 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
+  reactStrictMode: true,
   images: {
     domains: ['lh3.googleusercontent.com', 'graph.facebook.com'],
   },
 }
+
+module.exports = nextConfig;
